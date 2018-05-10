380b33f7d3b5680ba9a8799e970bdaedef6a9fa5
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2018-05-10 09:57:48 +0300

    first commit

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-print(12233)
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.python.org/events/python-events/'
+
+req = requests.get(url)
+
+print(req.text[:200])
+
+soup = BeautifulSoup(req.text, 'lxml')
